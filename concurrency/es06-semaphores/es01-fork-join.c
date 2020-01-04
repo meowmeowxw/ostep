77ef6028902bde1c489cc1b1aee9e7e43f7801c8
meowmeowxw <giovanni.disanti@protonmail.com>
2020-01-04 17:33:56 +0100

    semaphore fork join

---
+++ concurrency/es06-semaphores/es01-fork-join.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <time.h>
+#include "mythreads.h"
+
+sem_t s; 
+
+void *child(void *arg) 
+{
+	sleep(1);
+	printf("child\n");
+	sem_post(&s);
+	return NULL;
+}
+
+int main(int argc, char *argv[]) 
+{
+	pthread_t p;
+	printf("parent: begin\n");
+	sem_init(&s, 0, 0);
+	Pthread_create(&p, NULL, child, NULL);
+	sem_wait(&s);
+	printf("parent: end\n");
+	return 0;
+}
+
