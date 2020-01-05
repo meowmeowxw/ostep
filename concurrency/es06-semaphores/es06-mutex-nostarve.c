ca3a9f876a99aec99a74770834678d97e7f383c8
meowmeowxw <giovanni.disanti@protonmail.com>
2020-01-05 18:03:25 +0100

    es06 mutex nostarve concept

--- concurrency/es06-semaphores/es06-mutex-nostarve.c
+++ concurrency/es06-semaphores/es06-mutex-nostarve.c
@@ -16,18 +16,49 @@
 
 typedef __ns_mutex_t 
 {
+	int room1;
+	int room2;
+	sem_t mutex;
+	sem_t s1;
+	sem_t s2;
 } ns_mutex_t;
 
 void ns_mutex_init(ns_mutex_t *m)
 {
+
 }
 
 void ns_mutex_acquire(ns_mutex_t *m)
 {
+	sem_wait(&m->mutex);
+	m->room1++;
+	sem_post(&m->mutex);
+	sem_wait(&m->s1);
+		m->room2++;
+		sem_wait(&m->mutex);
+		m->room1--;
+		if(room1 == 0)
+		{
+			sem_post(&m->mutex);
+			sem_post(&m->s2);
+		} else
+		{
+			sem_post(&m->mutex);
+			sem_post(&m->s1);
+		}
+	sem_wait(&m->s2);
+		m->room2--;
 }
 
 void ns_mutex_release(ns_mutex_t *m) 
 {
+	if(m->room2 == 0)
+	{
+		sem_post(&m->s1);
+	} else
+	{
+		sem_post(&m->s2);
+	}
 }
 
 
