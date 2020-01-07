971029a0a8a599b757225f892309286cd5805427
meowmeowxw <giovanni.disanti@protonmail.com>
2020-01-07 17:22:14 +0100

    week04 class es02 optimization but need more tests

--- concurrency/class-exercises/week04/es02-producer-consumer-serial.c
+++ concurrency/class-exercises/week04/es02-producer-consumer-serial.c
@@ -9,27 +9,35 @@
 #define N 3
 #define M 5
 #define K 10
+#define A 0
+#define B 1
 
 int buffer_a;
 int buffer_b;
+int option;
 
+sem_t full;
 sem_t full_a;
 sem_t full_b;
 
+sem_t empty;
 sem_t empty_a;
 sem_t empty_b;
 
 sem_t mutex_a;
 sem_t mutex_b;
+sem_t mutex;
 
 void put_a(int a)
 {
 	buffer_a = a;
+	option = A;
 }
 
 void put_b(int b)
 {
 	buffer_b = b;
+	option = B;
 }
 
 int get_a()
@@ -42,6 +50,11 @@ int get_b()
 	return buffer_b;
 }
 
+int get_option()
+{
+	return option;
+}
+
 void *producer_a(void *arg)
 {
 	int x;
@@ -52,6 +65,7 @@ void *producer_a(void *arg)
 		put_a(x);
 		sem_post(&mutex_a);
 		sem_post(&full_a);
+		sem_post(&full);
 		sleep(1);
 	}
 	return NULL;
@@ -67,6 +81,7 @@ void *producer_b(void *arg)
 		put_b(x);
 		sem_post(&mutex_b);
 		sem_post(&full_b);
+		sem_post(&full);
 		sleep(1);
 	}
 	return NULL;
@@ -75,23 +90,42 @@ void *producer_b(void *arg)
 void *consumer(void *arg)
 {
 	int id = *(int *)arg;
-	int a, b;
+	int a, b, option;
 	while(1)
 	{
-		// get a
-		sem_wait(&full_a);
-		sem_wait(&mutex_a);
-		a = get_a();
-		sem_post(&mutex_a);
-		sem_post(&empty_a);
+		sem_wait(&full);
+		option = get_option();
+		if(option == A)
+		{
+			// get a
+			puts("a");
+			sem_wait(&full_a);
+			sem_wait(&mutex_a);
+			a = get_a();
+			sem_post(&mutex_a);
+			sem_post(&empty_a);
 
-		// get b
-		sem_wait(&full_b);
-		sem_wait(&mutex_b);
-		b = get_b();
-		sem_post(&mutex_b);
-		sem_post(&empty_b);
+			// get b
+			sem_wait(&full_b);
+			sem_wait(&mutex_b);
+			b = get_b();
+			sem_post(&mutex_b);
+			sem_post(&empty_b);
+		} else
+		{
+			// get b
+			sem_wait(&full_b);
+			sem_wait(&mutex_b);
+			b = get_b();
+			sem_post(&mutex_b);
+			sem_post(&empty_b);
 
+			sem_wait(&full_a);
+			sem_wait(&mutex_a);
+			a = get_a();
+			sem_post(&mutex_a);
+			sem_post(&empty_a);
+		}
 		printf("consumer: %d\ta:%d\tb:%d\n", id, a, b);
 		sleep(1);
 	}
@@ -109,6 +143,8 @@ int main(int argc, char **argv)
 	sem_init(&empty_b, 0, 1);
 	sem_init(&mutex_a, 0, 1);
 	sem_init(&mutex_b, 0, 1);
+	sem_init(&mutex, 0, 1);
+	sem_init(&full, 0, 0);
 	for(i = 0; i < N; i++)
 	{
 		Pthread_create(&th, NULL, producer_a, NULL);
