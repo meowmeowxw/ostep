8f62cb1927a3b070a38e0aa7948f57970b7d1051
meowmeowxw <giovanni.disanti@protonmail.com>
2020-01-15 19:10:20 +0100

    clang formatted header files

--- concurrency/es05-condition-variables/mythreads.h
+++ concurrency/es05-condition-variables/mythreads.h
@@ -1,10 +1,10 @@
 #ifndef __MYTHREADS_h__
 #define __MYTHREADS_h__
 
-#include <pthread.h>
 #include <assert.h>
-#include <sys/time.h>
+#include <pthread.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 void *Malloc(size_t size) {
     void *p = malloc(size);
@@ -16,13 +16,13 @@ double Time_GetSeconds() {
     struct timeval t;
     int rc = gettimeofday(&t, NULL);
     assert(rc == 0);
-    return (double) ((double)t.tv_sec + (double)t.tv_usec / 1e6);
+    return (double)((double)t.tv_sec + (double)t.tv_usec / 1e6);
 }
 
 void work(int seconds) {
     double t0 = Time_GetSeconds();
     while ((Time_GetSeconds() - t0) < (double)seconds)
-	;
+        ;
 }
 
 void Mutex_init(pthread_mutex_t *m) {
@@ -39,9 +39,7 @@ void Mutex_unlock(pthread_mutex_t *m) {
     assert(rc == 0);
 }
 
-void Cond_init(pthread_cond_t *c) {
-    assert(pthread_cond_init(c, NULL) == 0);
-}
+void Cond_init(pthread_cond_t *c) { assert(pthread_cond_init(c, NULL) == 0); }
 
 void Cond_wait(pthread_cond_t *c, pthread_mutex_t *m) {
     int rc = pthread_cond_wait(c, m);
@@ -53,9 +51,8 @@ void Cond_signal(pthread_cond_t *c) {
     assert(rc == 0);
 }
 
-
-void Pthread_create(pthread_t *thread, const pthread_attr_t *attr, 	
-		    void *(*start_routine)(void*), void *arg) {
+void Pthread_create(pthread_t *thread, const pthread_attr_t *attr,
+                    void *(*start_routine)(void *), void *arg) {
     int rc = pthread_create(thread, attr, start_routine, arg);
     assert(rc == 0);
 }
@@ -64,6 +61,5 @@ void Pthread_join(pthread_t thread, void **value_ptr) {
     int rc = pthread_join(thread, value_ptr);
     assert(rc == 0);
 }
-
 
 #endif // __MYTHREADS_h__
