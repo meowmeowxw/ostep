30bb075be2a6c80469108df59033a0f207bc15f7
meowmeowxw <giovanni.disanti@protonmail.com>
2020-01-15 17:31:14 +0100

    wish shell project parsing argument working

--- wish-shell/main.c
+++ wish-shell/main.c
@@ -1,12 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 #include <string.h>
 #include <time.h>
 #include <sys/wait.h>
-#include "lib.h"
 
-u_int8_t batch_mode = 0;
+#define MAX_ARGS 20
+
+void print_error(char *msg)
+{
+	fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+	exit(1);
+}
+
+void *Malloc(size_t size)
+{
+	void *ptr;
+	if((ptr = malloc(size)) == NULL)
+	{
+		print_error("malloc");
+	}
+	return ptr;
+}
+
+void **parse_line(char *line, char **arg)
+{
+	int count;
+	int index = 0;
+	while(*line != '\0' && *line != '\n')
+	{
+		count = 0;
+		while (*line == ' ' || *line == '\t' || *line == '\n')
+			*line++ = '\0';
+		while (*line != '\0' && *line != ' ' && *line != '\t' && *line != '\n') 
+		{
+		    count++;
+			line++;
+		}
+		if(index > MAX_ARGS)
+		{
+			puts("shit");
+			exit(1);
+		}
+		arg[index] = Malloc(count);
+		line -= count;
+		memcpy(arg[index], line, count);
+		line += count;
+		index++;
+	}
+}
+
+void execute_cd(char **arg)
+{
+	if(chdir(arg[1]) == -1)
+		print_error("cd");
+}
+
+void execute_path(char **arg)
+{
+	puts("path done");
+}
+
+int is_built_in(char *cmd)
+{
+	char *built_in[] = {"cd", "exit", "path"};
+	for(int i = 0; i < 3; i++)
+	{
+		if(strncmp(cmd, built_in[i], strlen(built_in[i])) == 0)
+			return 1;
+	}
+	return 0;
+}
 
 int main(int argc, char **argv)
 {
@@ -24,7 +89,6 @@ int main(int argc, char **argv)
 	}
 	if(argc == 2)
 	{
-		batch_mode = 1;
 		if((stream = fopen(argv[1], "r")) == NULL)
 			print_error("fopen");
 	}
@@ -32,17 +96,32 @@ int main(int argc, char **argv)
 	{
 		if((nread = getline(&line, &len, stream)) != -1)
 		{
-			if((pid = fork()) == -1)
-				print_error("fork");
-			if(pid == 0)
+			char *arg[MAX_ARGS] = {'\0'};
+			parse_line(line, arg);
+			if(!is_built_in(arg[0]))
 			{
-				puts("child");
-				exit(0);
+				if((pid = fork()) == -1)
+					print_error("fork");
+				if(pid == 0)
+				{
+					execvp(arg[0], arg);
+					exit(0);
+				} else
+				{
+					wait(NULL);
+				}
 			} else
 			{
-				wait(NULL);
-				puts("parent");
+				if(strcmp(arg[0], "cd") == 0)
+					execute_cd(arg);
+				if(strcmp(arg[0], "path") == 0)
+					execute_path(arg);
+				if(strcmp(arg[0], "exit") == 0)
+					exit(0);
 			}
+		} else
+		{
+			break;
 		}
 	}
 	free(line);
