e70fcacaddada714fa518ba5900ed25df6096a86
meowmeowxw <giovanni.disanti@protonmail.com>
2020-01-15 19:07:26 +0100

    persistence clang formatted

--- persistence/es03-files-and-directories/es02-ls.c
+++ persistence/es03-files-and-directories/es02-ls.c
@@ -1,110 +1,114 @@
+#include "error.h"
+#include <dirent.h>
+#include <getopt.h>
+#include <pwd.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <time.h>
-#include <dirent.h>
-#include <pwd.h>
-#include <getopt.h>
-#include <stdbool.h>
-#include <sys/types.h>
 #include <sys/stat.h>
-#include "error.h"
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
 
 #define MAX_ARGS 3
 #define MAX_LENGTH_NAME_FILE 1024
 
 int extended = 0;
 
-void print_file(struct stat *info)
-{
-	struct passwd *p;
-	p = getpwuid(info->st_uid);
-	switch(info->st_mode & S_IFMT)
-	{
-		case S_IFBLK:  printf("b"); break;
-		case S_IFCHR:  printf("c"); break;
-		case S_IFDIR:  printf("d"); break;
-		case S_IFIFO:  printf("f"); break;
-		case S_IFLNK:  printf("l"); break;
-		case S_IFREG:  printf("-"); break;
-		case S_IFSOCK: printf("s"); break;
-		default:       printf("?"); break;
-	}
-	printf((info->st_mode & S_IRUSR) ? "r" : "-");
-	printf((info->st_mode & S_IWUSR) ? "w" : "-");
-	printf((info->st_mode & S_IXUSR) ? "x" : "-");
-	printf((info->st_mode & S_IRGRP) ? "r" : "-");
-	printf((info->st_mode & S_IWGRP) ? "w" : "-");
-	printf((info->st_mode & S_IXGRP) ? "x" : "-");
-	printf((info->st_mode & S_IROTH) ? "r" : "-");
-	printf((info->st_mode & S_IWOTH) ? "w" : "-");
-	printf((info->st_mode & S_IXOTH) ? "x" : "-");
-	printf("\t%s\t%lu\t", p->pw_name, info->st_size);
-	return;
+void print_file(struct stat *info) {
+    struct passwd *p;
+    p = getpwuid(info->st_uid);
+    switch (info->st_mode & S_IFMT) {
+    case S_IFBLK:
+        printf("b");
+        break;
+    case S_IFCHR:
+        printf("c");
+        break;
+    case S_IFDIR:
+        printf("d");
+        break;
+    case S_IFIFO:
+        printf("f");
+        break;
+    case S_IFLNK:
+        printf("l");
+        break;
+    case S_IFREG:
+        printf("-");
+        break;
+    case S_IFSOCK:
+        printf("s");
+        break;
+    default:
+        printf("?");
+        break;
+    }
+    printf((info->st_mode & S_IRUSR) ? "r" : "-");
+    printf((info->st_mode & S_IWUSR) ? "w" : "-");
+    printf((info->st_mode & S_IXUSR) ? "x" : "-");
+    printf((info->st_mode & S_IRGRP) ? "r" : "-");
+    printf((info->st_mode & S_IWGRP) ? "w" : "-");
+    printf((info->st_mode & S_IXGRP) ? "x" : "-");
+    printf((info->st_mode & S_IROTH) ? "r" : "-");
+    printf((info->st_mode & S_IWOTH) ? "w" : "-");
+    printf((info->st_mode & S_IXOTH) ? "x" : "-");
+    printf("\t%s\t%lu\t", p->pw_name, info->st_size);
+    return;
 }
 
-int main(int argc, char **argv)
-{
-	struct dirent *d;
-	struct stat info;
-	DIR *dir;
-	char *path = ".";
-	char file_path[MAX_LENGTH_NAME_FILE];
-	int option_index;
-	opterr = 0;
+int main(int argc, char **argv) {
+    struct dirent *d;
+    struct stat info;
+    DIR *dir;
+    char *path = ".";
+    char file_path[MAX_LENGTH_NAME_FILE];
+    int option_index;
+    opterr = 0;
 
-	while((option_index = getopt(argc, argv, "l:")) != -1)
-	{
-		switch(option_index)
-		{
-			case 'l':
-				path = optarg;
-				extended = 1;
-				break;
-			case '?':
-				if(optopt == 'l')
-					extended = 1;
-				break;
-			default:
-				break;
-		}
-		
-	}
-	if(!extended && argc > 1)
-		path = argv[1];
+    while ((option_index = getopt(argc, argv, "l:")) != -1) {
+        switch (option_index) {
+        case 'l':
+            path = optarg;
+            extended = 1;
+            break;
+        case '?':
+            if (optopt == 'l')
+                extended = 1;
+            break;
+        default:
+            break;
+        }
+    }
+    if (!extended && argc > 1)
+        path = argv[1];
 
-	if((dir = opendir(path)) == NULL)
-		print_error();
-	
-	if(stat(path, &info) == -1)
-		print_error();
+    if ((dir = opendir(path)) == NULL)
+        print_error();
 
-	if(S_ISDIR(info.st_mode))
-	{
-		while((d = readdir(dir)) != NULL)
-		{
-			if(extended)
-			{
-				strncpy(file_path, path, strlen(path));
-				strcat(file_path, "/");
-				strncat(file_path, d->d_name, strlen(d->d_name));
-				if(stat(file_path, &info) == -1)
-				{
-					print_error();
-				}
-				print_file(&info);
-				memset(file_path, 0, MAX_LENGTH_NAME_FILE);
-			}
-			printf("%s\n", d->d_name);
-		}
-		closedir(dir);
-	} else
-	{
-		if(extended)
-			print_file(&info);
-		printf("%s\n", path);
-	}
-	return 0;
+    if (stat(path, &info) == -1)
+        print_error();
+
+    if (S_ISDIR(info.st_mode)) {
+        while ((d = readdir(dir)) != NULL) {
+            if (extended) {
+                strncpy(file_path, path, strlen(path));
+                strcat(file_path, "/");
+                strncat(file_path, d->d_name, strlen(d->d_name));
+                if (stat(file_path, &info) == -1) {
+                    print_error();
+                }
+                print_file(&info);
+                memset(file_path, 0, MAX_LENGTH_NAME_FILE);
+            }
+            printf("%s\n", d->d_name);
+        }
+        closedir(dir);
+    } else {
+        if (extended)
+            print_file(&info);
+        printf("%s\n", path);
+    }
+    return 0;
 }
-
--- persistence/es03-files-and-directories/es03-tail.c
+++ persistence/es03-files-and-directories/es03-tail.c
@@ -1,83 +1,75 @@
+#include "error.h"
+#include <fcntl.h>
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <time.h>
-#include <getopt.h>
-#include <fcntl.h>
 #include <sys/stat.h>
-#include "error.h"
+#include <time.h>
+#include <unistd.h>
 
-int main(int argc, char **argv)
-{
-	struct stat info;
-	char *path;
-	char *buf;
-	char chr[0];
-	int option_index;
-	int fd;
-	long int nlines = 0;
-	long unsigned int total_size = 0;
-	long unsigned int i = 0;
-	int length_line = 0;
-	opterr = 0;
-	
-	if(argc != 4)
-	{
-		fprintf(stderr, "Usage: %s -n lines filename\n", argv[0]);
-		exit(1);
-	}
+int main(int argc, char **argv) {
+    struct stat info;
+    char *path;
+    char *buf;
+    char chr[0];
+    int option_index;
+    int fd;
+    long int nlines = 0;
+    long unsigned int total_size = 0;
+    long unsigned int i = 0;
+    int length_line = 0;
+    opterr = 0;
 
-	while((option_index = getopt(argc, argv, "n:")) != -1)
-	{
-		switch(option_index)
-		{
-			case 'n':
-				nlines = atoll(optarg);
-				break;
-			default:
-				fprintf(stderr, "Usage: %s -n lines filename\n", argv[0]);
-				exit(1);
-		}
-	}
-	path = argv[optind];
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s -n lines filename\n", argv[0]);
+        exit(1);
+    }
 
-	if(stat(path, &info) == -1)
-		print_error();
-	
-	if((fd = open(path, O_RDONLY)) == -1)
-		print_error();
-	
-	if(lseek(fd, -1, SEEK_END) == -1)
-		print_error();
-	
-	while(nlines >= 0)
-	{
-		if(read(fd, chr, 1) == -1)
-			print_error();
+    while ((option_index = getopt(argc, argv, "n:")) != -1) {
+        switch (option_index) {
+        case 'n':
+            nlines = atoll(optarg);
+            break;
+        default:
+            fprintf(stderr, "Usage: %s -n lines filename\n", argv[0]);
+            exit(1);
+        }
+    }
+    path = argv[optind];
 
-		length_line++;
-		total_size++;
-		if(chr[0] == '\n')
-		{
-			length_line = 0;
-			nlines--;
-		}
-		if(lseek(fd, -2, SEEK_CUR) == -1)
-			break;
-	}
+    if (stat(path, &info) == -1)
+        print_error();
 
-	if((buf = malloc(total_size)) == NULL)
-	{
-		fprintf(stderr, "error malloc\n");
-		exit(1);
-	}
+    if ((fd = open(path, O_RDONLY)) == -1)
+        print_error();
 
-	lseek(fd, -(total_size), SEEK_END);
-	if(read(fd, buf, total_size) == -1)
-		print_error();
+    if (lseek(fd, -1, SEEK_END) == -1)
+        print_error();
 
-	printf("%s\n", buf);
-	return 0;
+    while (nlines >= 0) {
+        if (read(fd, chr, 1) == -1)
+            print_error();
+
+        length_line++;
+        total_size++;
+        if (chr[0] == '\n') {
+            length_line = 0;
+            nlines--;
+        }
+        if (lseek(fd, -2, SEEK_CUR) == -1)
+            break;
+    }
+
+    if ((buf = malloc(total_size)) == NULL) {
+        fprintf(stderr, "error malloc\n");
+        exit(1);
+    }
+
+    lseek(fd, -(total_size), SEEK_END);
+    if (read(fd, buf, total_size) == -1)
+        print_error();
+
+    printf("%s\n", buf);
+    return 0;
 }
-
