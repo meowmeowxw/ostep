66a30c2a3543fe0a5f94578f472f8d6b00a5c7eb
meowmeowxw <giovanni.disanti@protonmail.com>
2020-01-15 21:26:13 +0100

    redirection stdout/stderr to improve wish shell

--- wish-shell/main.c
+++ wish-shell/main.c
@@ -1,12 +1,30 @@
+#define _GNU_SOURCE
+
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 
 #define MAX_ARGS 20
+
+int arg_count = 0;
+char *path_output = NULL;
+char *path_error = NULL;
+
+void print_error(char *msg);
+void *Malloc(size_t size);
+void parse_line(char *line, char **arg);
+void execute_path(char **arg);
+void execute_cd(char **arg);
+int is_built_int(char *cmd);
+void execute_cmd(char **arg);
+int check_redirection(char **arg);
+void reduce_arg(char **arg);
 
 void print_error(char *msg) {
     fprintf(stderr, "%s: %s\n", msg, strerror(errno));
@@ -21,9 +39,10 @@ void *Malloc(size_t size) {
     return ptr;
 }
 
-void **parse_line(char *line, char **arg) {
+void parse_line(char *line, char **arg) {
     int count;
     int index = 0;
+	arg_count = 0;
     while (*line != '\0' && *line != '\n') {
         count = 0;
         while (*line == ' ' || *line == '\t' || *line == '\n') {
@@ -44,14 +63,18 @@ void **parse_line(char *line, char **arg) {
         line += count;
         index++;
     }
+	arg_count = index;
 }
 
 void execute_cd(char **arg) {
-    if (chdir(arg[1]) == -1)
+    if (chdir(arg[1]) == -1) {
         print_error("cd");
+	}
 }
 
-void execute_path(char **arg) { puts("path done"); }
+void execute_path(char **arg) { 
+	puts("path done"); 
+}
 
 int is_built_in(char *cmd) {
     char *built_in[] = {"cd", "exit", "path"};
@@ -66,11 +89,33 @@ int is_built_in(char *cmd) {
 void execute_cmd(char **arg) {
     pid_t pid;
     int status;
-
     if ((pid = fork()) == -1) {
         print_error("fork");
     }
     if (pid == 0) {
+		if(check_redirection(arg) != -1) {
+			if(path_error != NULL) {
+				int f_error;
+				if((f_error = open(path_error, O_CREAT | O_WRONLY)) == -1) {
+					print_error("open");
+				}
+				if(dup2(f_error, 2) == -1) {
+					print_error("dup2");
+				}
+				close(f_error);
+			}
+			if(path_output != NULL) {
+				int f_output;
+				if((f_output = open(path_output, O_CREAT | O_WRONLY)) == -1) {
+					print_error("open");
+				}
+				if(dup2(f_output, 1) == -1) {
+					print_error("dup2");
+				}
+				close(f_output);
+			}
+		}
+		reduce_arg(arg);
         if (execvp(arg[0], arg) == -1) {
             print_error("execvp");
         }
@@ -82,14 +127,52 @@ void execute_cmd(char **arg) {
     }
 }
 
+void reduce_arg(char **arg)
+{
+	for(int i = arg_count; i < sizeof(arg); i++) {
+		free(arg[i]);
+		arg[i] = NULL;
+	}
+}
+void print_prompt()
+{
+	char *cwd = get_current_dir_name();
+	printf("%s\n", cwd);
+	free(cwd);
+	printf("$ ");
+}
+
+int check_redirection(char **arg)
+{
+	int i, j;
+	int quote = 0;
+	for(i = 0; i < arg_count; i++)	{
+		for(j = 0; j < strlen(arg[i]); j++) {
+			if(arg[i][j] == '"') {
+				quote = (quote + 1) % 2;
+			}
+			if(arg[i][j] == '>' && quote == 0) {
+				if(arg[i][j-1] == '2') {
+					path_error = arg[i + 1];
+				} else {
+					path_output = arg[i + 1];
+				}
+				arg_count -= 2;
+				return i;
+			}
+		}
+	}
+	return -1;
+}
+
 int main(int argc, char **argv) {
-    char prompt[] = "prompt> ";
+    char **arg;
     FILE *stream = stdin;
     char *line = NULL;
     ssize_t len = 0;
     ssize_t nread;
     int pid;
-    char **arg;
+
     if (argc > 2) {
         fprintf(stderr, "usage: %s [batch-file]\n", argv[0]);
         exit(2);
@@ -100,7 +183,7 @@ int main(int argc, char **argv) {
         }
     }
     while (1) {
-        printf("%s", prompt);
+		print_prompt();
         if ((nread = getline(&line, &len, stream)) != -1) {
             arg = calloc(MAX_ARGS, 8);
             parse_line(line, arg);
