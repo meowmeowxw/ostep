4fab604a872a61ef0d5e7fca1f6fe6227dd66070
meowmeowxw <giovanni.disanti@protonmail.com>
2020-01-15 22:51:00 +0100

    fix tab spaces wish

--- wish-shell/wish.c
+++ wish-shell/wish.c
@@ -27,48 +27,48 @@ int check_redirection(char **arg);
 void reduce_arg(char **arg);
 
 void print_error(char *msg) {
-    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
-    exit(1);
+	fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+	exit(1);
 }
 
 void *Malloc(size_t size) {
-    void *ptr;
-    if ((ptr = malloc(size)) == NULL) {
-        print_error("malloc");
-    }
-    return ptr;
+	void *ptr;
+	if ((ptr = malloc(size)) == NULL) {
+		print_error("malloc");
+	}
+	return ptr;
 }
 
 void parse_line(char *line, char **arg) {
-    int count;
-    int index = 0;
+	int count;
+	int index = 0;
 	arg_count = 0;
-    while (*line != '\0' && *line != '\n') {
-        count = 0;
-        while (*line == ' ' || *line == '\t' || *line == '\n') {
-            *line++ = '\0';
-        }
-        while (*line != '\0' && *line != ' ' && *line != '\t' &&
-               *line != '\n') {
-            count++;
-            line++;
-        }
-        if (index > MAX_ARGS) {
-            puts("shit");
-            exit(1);
-        }
-        arg[index] = Malloc(count);
-        line -= count;
-        memcpy(arg[index], line, count);
-        line += count;
-        index++;
-    }
+	while (*line != '\0' && *line != '\n') {
+		count = 0;
+		while (*line == ' ' || *line == '\t' || *line == '\n') {
+			*line++ = '\0';
+		}
+		while (*line != '\0' && *line != ' ' && *line != '\t' &&
+				*line != '\n') {
+			count++;
+			line++;
+		}
+		if (index > MAX_ARGS) {
+			puts("shit");
+			exit(1);
+		}
+		arg[index] = Malloc(count);
+		line -= count;
+		memcpy(arg[index], line, count);
+		line += count;
+		index++;
+	}
 	arg_count = index;
 }
 
 void execute_cd(char **arg) {
-    if (chdir(arg[1]) == -1) {
-        print_error("cd");
+	if (chdir(arg[1]) == -1) {
+		print_error("cd");
 	}
 }
 
@@ -77,22 +77,22 @@ void execute_path(char **arg) {
 }
 
 int is_built_in(char *cmd) {
-    char *built_in[] = {"cd", "exit", "path"};
-    for (int i = 0; i < 3; i++) {
-        if (strncmp(cmd, built_in[i], strlen(built_in[i])) == 0) {
-            return 1;
-        }
-    }
-    return 0;
+	char *built_in[] = {"cd", "exit", "path"};
+	for (int i = 0; i < 3; i++) {
+		if (strncmp(cmd, built_in[i], strlen(built_in[i])) == 0) {
+			return 1;
+		}
+	}
+	return 0;
 }
 
 void execute_cmd(char **arg) {
-    pid_t pid;
-    int status;
-    if ((pid = fork()) == -1) {
-        print_error("fork");
-    }
-    if (pid == 0) {
+	pid_t pid;
+	int status;
+	if ((pid = fork()) == -1) {
+		print_error("fork");
+	}
+	if (pid == 0) {
 		if(check_redirection(arg) != -1) {
 			if(path_error != NULL) {
 				int f_error;
@@ -116,15 +116,15 @@ void execute_cmd(char **arg) {
 			}
 		}
 		reduce_arg(arg);
-        if (execvp(arg[0], arg) == -1) {
-            print_error("execvp");
-        }
-        exit(0);
-    } else {
-        waitpid(pid, &status, 0);
-        int exit_status = WEXITSTATUS(status);
-        printf("%u  \n", exit_status);
-    }
+		if (execvp(arg[0], arg) == -1) {
+			print_error("execvp");
+		}
+		exit(0);
+	} else {
+		waitpid(pid, &status, 0);
+		int exit_status = WEXITSTATUS(status);
+		printf("%u  \n", exit_status);
+	}
 }
 
 void reduce_arg(char **arg)
@@ -175,48 +175,47 @@ void del(char **arg) {
 }
 
 int main(int argc, char **argv) {
-    char **arg;
-    FILE *stream = stdin;
-    char *line = NULL;
-    ssize_t len = 0;
-    ssize_t nread;
-    int pid;
+	char **arg;
+	FILE *stream = stdin;
+	char *line = NULL;
+	ssize_t len = 0;
+	ssize_t nread;
+	int pid;
 
-    if (argc > 2) {
-        fprintf(stderr, "usage: %s [batch-file]\n", argv[0]);
-        exit(2);
-    }
-    if (argc == 2) {
-        if ((stream = fopen(argv[1], "r")) == NULL) {
-            print_error("fopen");
-        }
-    }
-    while (1) {
+	if (argc > 2) {
+		fprintf(stderr, "usage: %s [batch-file]\n", argv[0]);
+		exit(2);
+	}
+	if (argc == 2) {
+		if ((stream = fopen(argv[1], "r")) == NULL) {
+			print_error("fopen");
+		}
+	}
+	while (1) {
 		path_output = NULL;
 		path_error = NULL;
 		print_prompt();
-        if ((nread = getline(&line, &len, stream)) != -1) {
-            arg = calloc(MAX_ARGS, sizeof(intptr_t));
-            parse_line(line, arg);
-            if (!is_built_in(arg[0])) {
-                execute_cmd(arg);
-            } else {
-                if (strcmp(arg[0], "cd") == 0) {
-                    execute_cd(arg);
-                }
-                if (strcmp(arg[0], "path") == 0) {
-                    execute_path(arg);
-                }
-                if (strcmp(arg[0], "exit") == 0) {
-                    exit(0);
-                }
-            }
-            free(arg);
-        } else {
-            break;
-        }
-    }
-    free(line);
-    fclose(stream);
-    return 0;
+		if ((nread = getline(&line, &len, stream)) != -1) {
+			arg = calloc(MAX_ARGS, sizeof(intptr_t));
+			parse_line(line, arg);
+			if (!is_built_in(arg[0])) {
+				execute_cmd(arg);
+			} else {
+				if (strcmp(arg[0], "cd") == 0) {
+					execute_cd(arg);
+				}
+				if (strcmp(arg[0], "path") == 0) {
+					execute_path(arg);
+				}
+				if (strcmp(arg[0], "exit") == 0) {
+					exit(0);
+				}
+			}
+			free(arg);
+		} else {
+			break;
+		}
+	}
+	free(line); fclose(stream);
+	return 0;
 }
