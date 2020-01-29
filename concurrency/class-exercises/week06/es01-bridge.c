5019059b20cc147a4f6be503ae531c541b9170e7
meowmeowxw <giovanni.disanti@protonmail.com>
2020-01-29 18:19:06 +0100

    es01 week06 bridge simple clang formatted

--- concurrency/class-exercises/week06/es01-bridge.c
+++ concurrency/class-exercises/week06/es01-bridge.c
@@ -1,9 +1,9 @@
+#include <mythreads.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
 #include <time.h>
-#include <mythreads.h>
+#include <unistd.h>
 
 #define NUM_CARS 8
 #define A 0
@@ -21,7 +21,7 @@ void *bridge(void *arg) {
     setbuf(stdout, NULL);
     sleep(2);
     int side;
-    while(1) {
+    while (1) {
         side = A;
         Pthread_mutex_lock(&mutex_queue);
         if (queue[A] < queue[B]) {
@@ -32,8 +32,8 @@ void *bridge(void *arg) {
         }
         ticket_go[side]++;
         queue[side]--;
-        printf("\nticket %d on point %c can pass\n", ticket_go[side], 
-                point[side]);
+        printf("\nticket %d on point %c can pass\n", ticket_go[side],
+               point[side]);
         free_bridge = 1;
         Pthread_cond_broadcast(&turn[side]);
         Pthread_mutex_unlock(&mutex_queue);
@@ -46,21 +46,22 @@ void *car(void *arg) {
     int id = *(int *)arg;
     int side = id % 2;
     int myticket;
-    while(1) {
+    while (1) {
         Pthread_mutex_lock(&mutex_queue);
         myticket = ++ticket[side];
-        printf("car[%d]\ttake ticket %d\tpoint %c\n", id, myticket, 
-                point[side]);
-        //Pthread_mutex_unlock(&mutex_ticket);
+        printf("car[%d]\ttake ticket %d\tpoint %c\n", id, myticket,
+               point[side]);
+        // Pthread_mutex_unlock(&mutex_ticket);
 
-        //Pthread_mutex_lock(&mutex_queue);
+        // Pthread_mutex_lock(&mutex_queue);
         queue[side]++;
         while (myticket != ticket_go[side] || !free_bridge) {
-            Pthread_cond_wait(&turn[side], &mutex_queue); 
+            Pthread_cond_wait(&turn[side], &mutex_queue);
         }
         free_bridge = 0;
         printf("car[%d] with ticket %d crosses the bridge from point %c to\
-point %c\n", id, myticket, point[side], point[(side + 1) % 2]);
+ point %c\n",
+               id, myticket, point[side], point[(side + 1) % 2]);
         sleep(1);
         Pthread_cond_signal(&ready);
         Pthread_mutex_unlock(&mutex_queue);
@@ -75,9 +76,9 @@ int main(int argc, char **argv) {
     setbuf(stdout, NULL);
     int i;
     int *x = malloc(sizeof(int) * NUM_CARS);
-    assert (x != NULL);
+    assert(x != NULL);
     pthread_t th[NUM_CARS + 1];
-    
+
     Pthread_cond_init(&ready, NULL);
     Pthread_cond_init(&turn[0], NULL);
     Pthread_cond_init(&turn[1], NULL);
@@ -85,15 +86,14 @@ int main(int argc, char **argv) {
     Pthread_mutex_init(&mutex_ticket, NULL);
 
     Pthread_create(&th[NUM_CARS], NULL, bridge, NULL);
-    for(i = 0; i < 8; i++) {
+    for (i = 0; i < 8; i++) {
         x[i] = i;
         Pthread_create(&th[i], NULL, car, &x[i]);
     }
     Pthread_join(th[NUM_CARS], NULL);
-    for(i = 0; i < 8; i++) {
+    for (i = 0; i < 8; i++) {
         Pthread_join(th[i], NULL);
     }
     free(x);
-	return 0;
+    return 0;
 }
-
